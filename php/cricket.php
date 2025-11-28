d1bc45cefd4314dae375e08fb9288a1ee4b26e4b
Kashyap RB <kashyapmadathil@gmail.com>
2025-11-28 11:25:46 +0530

    initial commit

--- php/cricket.php
+++ php/cricket.php
@@ -1,41 +1,50 @@
 <html>
 <head>
-<title>INDIAN CRICKET PLAYERS</title>
-<style>
-table{
-width:50%;
-border-collapse:collapse;
-margin:20px auto;
-}
-th,td{
-padding:10px;
-text-align:center;
-border:1px solid black;
-}
-th{
-backgroud-color:lightgrey;
-}
-</style>
+    <title>Indian Cricket Players</title>
+    <style>
+        table {
+            width: 50%;
+            border-collapse: collapse;
+            margin: 20px auto;
+        }
+        th, td {
+            padding: 10px;
+            text-align: center;
+            border: 1px solid black;
+        }
+        th {
+            background-color: lightgrey;
+        }
+    </style>
 </head>
+
 <body>
-<h2 style="text-aling:center;"><center>LIST OF INDIAN CRICKET PLAYERS</center></h2>
-<?php
-$players=array(
-"VIRAT KOHLI",
-"ROHIT SHARMA",
-"SACHIN TENDULKAR",
-"MS DHONI"
+    <h2 style="text-align: center;">List of Indian Cricket Players</h2>    
+    <?php
+   
+
+
+
+
+ $players = array(
+        "Virat Kohli",
+        "Rohit Sharma",
+        "Sachin Tendulkar",
+        "MS Dhoni",
+        "Shikhar Dhawan",
+        "KL Rahul",
+        "Jadeja",
+        "Hardik Pandya",
+        "Bhuvneshwar Kumar",
+        "Ishant Sharma"
 );
-echo"<table>";
-echo"<tr><th>S.NO</th><th>players name</th></tr>";
-foreach($players as $x=>$player){
-echo"<tr><td>".($x+1)."</td><td>".$player."</td></tr>";
-}
-echo"</table>";
-?>
+
+    echo "<table>";
+    echo "<tr><th>S.No</th><th>Player Name</th></tr>"; 
+    foreach ($players as $x => $player) {
+        echo "<tr><td>" . ($x + 1) . "</td><td>" . $player . "</td></tr>";
+    }
+    echo "</table>";
+    ?>
 </body>
 </html>
-
-
-
-
